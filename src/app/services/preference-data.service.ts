720407ea86330da2e7cfd9d1f75f0f9e811b831e
devang-ansorbit <devang.patel@ansorbit.com>
2023-03-29 12:21:49 +0530

    code update

--- src/app/services/preference-data.service.ts
+++ src/app/services/preference-data.service.ts
@@ -6,12 +6,12 @@ import { HttpClient } from '@angular/common/http';
 })
 export class PreferenceDataService {
 
-  baseUrl: string = 'http://localhost:8000/'
+  baseUrl: string = 'http://localhost:8000'
 
   constructor( private http : HttpClient) { }
 
   getPreferenceData(){
     console.log(" iam from prefrence service")
-    return this.http.get(this.baseUrl + 'prefrence-data');
+    return this.http.get(`${this.baseUrl}/prefrence-data`);
   }
 }
